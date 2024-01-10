86a92623a8a3c0d49d30f60798a2024fd448877e
haneki <hanekihyouka@gmail.com>
2024-01-10 21:52:30 +0800

    add a leaderboard

--- src/main/java/haneki/cloverclear/command/CmdTrashCan.java
+++ src/main/java/haneki/cloverclear/command/CmdTrashCan.java
@@ -4,6 +4,8 @@ import com.feed_the_beast.ftblib.lib.command.CmdBase;
 import haneki.cloverclear.util.ClearUtil;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.scoreboard.*;
 import net.minecraft.server.MinecraftServer;
 
 public class CmdTrashCan extends CmdBase{
@@ -13,6 +15,15 @@ public class CmdTrashCan extends CmdBase{
 
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-        getCommandSenderAsPlayer(sender).displayGUIChest(ClearUtil.trashcan);
+        if (sender instanceof EntityPlayer){
+            EntityPlayer player = getCommandSenderAsPlayer(sender);
+            player.displayGUIChest(ClearUtil.trashcan);
+            Scoreboard scoreboard = player.getWorldScoreboard();
+            if (scoreboard.getObjective("scavenger")==null){
+                scoreboard.addScoreObjective("scavenger",ScoreCriteria.DUMMY);
+            }
+            Score score = scoreboard.getOrCreateScore(player.getName(),scoreboard.getObjective("scavenger"));
+            score.increaseScore(1);
+        }
     }
 }
